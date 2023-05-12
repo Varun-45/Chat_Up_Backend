644d29882288040593bb8b05e0dde5f05ca938a6
unknown <varunmalpani.vm01@gmail.com>
2023-05-12 16:41:15 +0530

    chaanged origin

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const server = app.listen(Port, console.log(`server started on port ${Port}`))
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chatsup.vercel.app/",
     }
 })
 
